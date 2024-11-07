d2c3d1a79e818c2e1ce9b9c2049468b6a0d4792d
Selsanat <leandro.benites@edu.devinci.fr>
2024-11-07 12:05:53 +0100

    Slide 30

--- Source/SmashUE/Private/Characters/SmashCharacter.cpp
+++ Source/SmashUE/Private/Characters/SmashCharacter.cpp
@@ -3,6 +3,8 @@
 
 #include "Characters/SmashCharacter.h"
 
+#include "Characters/SmashCharacterStateMachine.h"
+
 
 // Sets default values
 ASmashCharacter::ASmashCharacter()
@@ -15,7 +17,9 @@ ASmashCharacter::ASmashCharacter()
 void ASmashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	CreateStateMachine();
+
+	InitStateMachine();
 }
 
 // Called every frame
@@ -48,4 +52,13 @@ void ASmashCharacter::RotateMeshUsingOrientX() const
 	GetMesh()->SetRelativeRotation(Rotation);
 }
 
+void ASmashCharacter::CreateStateMachine()
+{
+	StateMachine = NewObject<USmashCharacterStateMachine>(this);
+}
 
+void ASmashCharacter::InitStateMachine()
+{
+	if (StateMachine == nullptr) return;
+	StateMachine->Init(this);
+}
---
+++ Source/SmashUE/Private/Characters/States/SmashCharacterStateWalk.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/States/SmashCharacterStateWalk.h"
+
+#include "Characters/SmashCharacterStateID.h"
+
+ESmashCharacterStateID USmashCharacterStateWalk::GetStateID()
+{
+	return ESmashCharacterStateID::Walk;
+}
+
+
